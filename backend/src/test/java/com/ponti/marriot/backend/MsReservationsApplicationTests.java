558424277f625193ee0e559da37282a644c22987
Nidhood <ivan.oroz34@gmail.com>
2025-10-05 16:47:04 -0500

    Agregar configuración inicial del backend con Spring Boot y archivos de Maven

---
+++ backend/src/test/java/com/ponti/marriot/backend/MsReservationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ponti.marriot.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsReservationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
